202cba7a8f6864d82aeb37cfa32adf3990babe4b
Regulo Argotte <rargotte@hotmail.com>
2022-02-28 21:41:40 -0600

    Conection to AWS

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: 'blog-db.cupivc3rrcux.sa-east-1.rds.amazonaws.com',
       dialect: 'mysql',
       port: 3306
     }
